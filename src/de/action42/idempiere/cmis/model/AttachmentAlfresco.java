828ce9a59806fc1e5df7c929192767639a297459
a42niem <a42niem@localhost>
2016-06-15 21:48:02 +0200

    Reworked to use versioning and checksum to identify differences

--- src/de/action42/idempiere/cmis/model/AttachmentAlfresco.java
+++ src/de/action42/idempiere/cmis/model/AttachmentAlfresco.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -25,6 +26,7 @@ import org.apache.chemistry.opencmis.client.api.Folder;
 import org.apache.chemistry.opencmis.client.api.Session;
 import org.apache.chemistry.opencmis.commons.exceptions.CmisObjectNotFoundException;
 import org.compiere.model.IAttachmentStore;
+import org.compiere.model.I_AD_Attachment;
 import org.compiere.model.MAttachment;
 import org.compiere.model.MAttachmentEntry;
 import org.compiere.model.MStorageProvider;
@@ -40,17 +42,26 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
- * @author a42niem
+ * @author a42niem - Dirk Niemeyer - action42 GmbH
  * 
- * based on work by kittiu
- *
+ * based on work done by kittiu for Alfresco 4 (see http://www.adempiere.com/index.php?title=ADempiere_Integration_With_Alfresco) 
+ * adapted to Alfresco 5 and extended
+ * 
  */
+
 public class AttachmentAlfresco implements IAttachmentStore {
 
 	// CMIS by KTU
+	// Adapted to Alfresco 5 and enhanced by Dirk Niemeyer / a42niem
 
 	private final CLogger log = CLogger.getCLogger(getClass());
 
+	Node m_docidNode = null;
+	Node m_fileNode = null;
+	Node m_nameNode = null;
+	Node m_versionNode = null;
+	Node m_md5Node = null;
+	
 	/**
 	 * 	Load Data from CMIS
 	 *	@return true if success
@@ -76,46 +87,50 @@ public class AttachmentAlfresco implements IAttachmentStore {
 				final Document document = builder.parse(new ByteArrayInputStream(data));
 				final NodeList entries = document.getElementsByTagName("entry");
 				for (int i = 0; i < entries.getLength(); i++) {
-					final Node entryNode = entries.item(i);
-					final NamedNodeMap attributes = entryNode.getAttributes();
-					final Node docidNode = attributes.getNamedItem("docid");
-					final Node fileNode = attributes.getNamedItem("file");
-					final Node nameNode = attributes.getNamedItem("name");
-					if(docidNode == null || fileNode==null || nameNode==null){
+					getEntryData(entries.item(i));
+					if(m_docidNode == null || m_fileNode==null || m_nameNode==null){
 						log.severe("no filename for entry " + i);
 						attach.m_items = null;
 						return false;
 					}
-					log.fine("name: " + nameNode.getNodeValue());
-					String docId = docidNode.getNodeValue();
-					//docID = m_attachmentViewerUrlCMIS + docID;
+					log.fine("name: " + m_nameNode.getNodeValue());
+					String docId = m_docidNode.getNodeValue();
+					if (m_versionNode != null) {
+						docId = docId + ";" + m_versionNode.getNodeValue();
+					}
 					log.fine("docId: " + docId);
-					
+
+
 					org.apache.chemistry.opencmis.client.api.Document file;
 					try
-					{
+					{	
 						// Check document on CMIS
-						List<org.apache.chemistry.opencmis.client.api.Document> versions;
 						org.apache.chemistry.opencmis.client.api.Document version = 
 								(org.apache.chemistry.opencmis.client.api.Document) session.getObject(session.createObjectId(docId));
-						versions = version.getAllVersions();
-						// Get only latest version
-						file = versions.get(0);
+						if (m_versionNode==null) {
+							List<org.apache.chemistry.opencmis.client.api.Document> versions;
+							versions = version.getAllVersions();
+							// Get only latest version
+							file = versions.get(0);
+						}
+						else {
+							file = version;
+						}
 					}
 					catch (CmisObjectNotFoundException e)
 					{
 						log.severe(e.getMessage());
 						file = null;
 					}
-					
+
 					if (file != null) {
-						
+
 						try {
 							InputStream fileInputStream = file.getContentStream().getStream();
 							final byte[] dataEntry = CmisUtil.toByteArray(fileInputStream);
 							fileInputStream.close();
 							String name = file.getName();
-							final MAttachmentEntry entry = new MAttachmentEntry(setDocId(name, docId), dataEntry, attach.m_items.size() + 1);
+							final MAttachmentEntry entry = new MAttachmentEntry(name, dataEntry, attach.m_items.size() + 1);
 							attach.m_items.add(entry);
 						} catch (FileNotFoundException e) {
 							log.severe("File Not Found.");
@@ -127,7 +142,7 @@ public class AttachmentAlfresco implements IAttachmentStore {
 					} else {
 						log.severe("Document ID not found: " + docId);
 					}
-					
+
 				}
 
 			} catch (SAXException sxe) {
@@ -152,7 +167,6 @@ public class AttachmentAlfresco implements IAttachmentStore {
 			return true;
 	}
 
-	// CMIS by KTU
 	/**
 	 * 	Save Entry Data to the CMIS
 	 *	@return true if saved
@@ -171,81 +185,177 @@ public class AttachmentAlfresco implements IAttachmentStore {
 		String cmisRootFolder = prov.getFolder();
 
 		final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			Session session = CmisUtil.createCmisSession(cmisUser, cmisPassword, m_attachmentCMISUrl);
-			try {
-				final DocumentBuilder builder = factory.newDocumentBuilder();
-				final Document document = builder.newDocument();
-				final Element root = document.createElement("attachments");
-				document.appendChild(root);
-				document.setXmlStandalone(true);
-				
-				// Get the source window information, to be saved as alfresco's document properties.
-				String tableName = MTable.getTableName(Env.getCtx(), attach.getAD_Table_ID());
-				String recordId = ((Integer)attach.getRecord_ID()).toString();
-				
-				// create xml entries
-				for (int i = 0; i < attach.m_items.size(); i++) {
-					log.fine(attach.m_items.get(i).toString());
-					
-					MAttachmentEntry item = attach.m_items.get(i);
-					String docId = getDocId(item);
-					
-					if (docId.equals("")) // Not exists, create new document.
-					{
-						File entryFile = attach.m_items.get(i).getFile();
-						final String path = entryFile.getAbsolutePath();
-						org.apache.chemistry.opencmis.client.api.Document cmisDoc = null;
-						// if local file - copy to central attachment folder
-						log.fine(path + " - " + m_attachmentCMISUrl);
+		Session session = CmisUtil.createCmisSession(cmisUser, cmisPassword, m_attachmentCMISUrl);
+		try {
+			final DocumentBuilder builder = factory.newDocumentBuilder();
+			final Document document = builder.newDocument();
+			final Element root = document.createElement("attachments");
+			document.appendChild(root);
+			document.setXmlStandalone(true);
 
-						log.fine("move file: " + path);
-						String mimeType = null;
-						byte[] content = null;
-						try {
-							// Prepare file / folder
-							content = item.getData();
-							mimeType = item.getContentType();
-							Folder folder = (Folder) CmisUtil.getFolder(session, cmisRootFolder);
-							if (folder == null)
-								throw new FolderNotFoundException();
-							
-							// Upload to CMIS
-							cmisDoc = CmisUtil.createiDempiereAttachment(session, folder, attach.getEntryName(i), mimeType, content, tableName, recordId);
-							docId = cmisDoc.getId().substring(0, cmisDoc.getId().lastIndexOf(";")); // Remove ";<version>"
-							
-						} catch (IOException e) {
-							e.printStackTrace();
-							log.severe("unable to copy file " + entryFile.getAbsolutePath() + " to "
-									+ m_attachmentCMISUrl + File.separator + 
-									getAttachmentPathSnippet(attach) + File.separator + entryFile.getName());
-						} 
-					}
-					final Element entry = document.createElement("entry");
-					//entry.setAttribute("name", m_items.get(i).getName());
-					entry.setAttribute("name", attach.getEntryName(i));
-					entry.setAttribute("file", attach.getEntryName(i)); // File
-					entry.setAttribute("docid", docId);
-					root.appendChild(entry);
+			// Get the source window information, to be saved as alfresco's document properties.
+			String tableName = MTable.getTableName(Env.getCtx(), attach.getAD_Table_ID());
+			String recordId = ((Integer)attach.getRecord_ID()).toString();
+
+			// create xml entries
+			for (int i = 0; i < attach.m_items.size(); i++) {
+				log.fine(attach.m_items.get(i).toString());
+
+				MAttachmentEntry item = attach.m_items.get(i);
+				getEntryData((byte[])attach.get_ValueOld(I_AD_Attachment.COLUMNNAME_BinaryData), i);
+				String docId =  (m_docidNode!=null) ? m_docidNode.getNodeValue() : "";
+				String docVersion = (m_versionNode!=null) ? m_versionNode.getNodeValue() : "";
+				String docMd5 = (m_md5Node!=null) ? m_md5Node.getNodeValue() : "";
+				byte[] content = item.getData();
+				MessageDigest md5Digest = MessageDigest.getInstance("MD5");
+				String checksum = getFileChecksum(md5Digest, content);
+				
+				// If not exists or is different, create or update document.
+				if (docId.equals("") || !docMd5.equals(checksum)) 
+				{
+					File entryFile = attach.m_items.get(i).getFile();
+					final String path = entryFile.getAbsolutePath();
+					org.apache.chemistry.opencmis.client.api.Document cmisDoc = null;
+					// if local file - copy to central attachment folder
+					log.fine(path + " - " + m_attachmentCMISUrl);
+
+					log.fine("move file: " + path);
+					String mimeType = null;
+					try {
+						// Prepare file / folder
+						mimeType = item.getContentType();
+						Folder folder = (Folder) CmisUtil.getFolder(session, cmisRootFolder);
+						if (folder == null)
+							throw new FolderNotFoundException();
+
+						// Upload to CMIS
+						cmisDoc = CmisUtil.createiDempiereAttachment(session, folder, attach.getEntryName(i), mimeType, content, tableName, recordId, checksum);
+						docId = cmisDoc.getId().substring(0, cmisDoc.getId().lastIndexOf(";")); // Remove ";<version>"
+						docVersion = cmisDoc.getId().substring(cmisDoc.getId().lastIndexOf(";")+1); // Get "<version>"
+						docMd5 = checksum;
+					} catch (IOException e) {
+						e.printStackTrace();
+						log.severe("unable to copy file " + entryFile.getAbsolutePath() + " to "
+								+ m_attachmentCMISUrl + File.separator + 
+								getAttachmentPathSnippet(attach) + File.separator + entryFile.getName());
+					} 
 				}
-
-				final Source source = new DOMSource(document);
-				final ByteArrayOutputStream bos = new ByteArrayOutputStream();
-				final Result result = new StreamResult(bos);
-				final Transformer xformer = TransformerFactory.newInstance().newTransformer();
-				xformer.transform(source, result);
-				final byte[] xmlData = bos.toByteArray();
-				log.fine(bos.toString());
-				attach.setBinaryData(xmlData);
-				return true;
-			} catch (FolderNotFoundException e) {
-				log.log(Level.SEVERE, "CMIS Root Folder Not Found!");
-			} catch (Exception e) {
-				log.log(Level.SEVERE, "saveLOBData", e);
+				final Element entry = document.createElement("entry");
+				entry.setAttribute("name", attach.getEntryName(i));
+				entry.setAttribute("file", attach.getEntryName(i)); // File
+				entry.setAttribute("docid", docId);
+				entry.setAttribute("version", docVersion);
+				entry.setAttribute("md5", docMd5);
+				root.appendChild(entry);
 			}
-			session = null;
-			attach.setBinaryData(null);
-			return false;
+
+			final Source source = new DOMSource(document);
+			final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			final Result result = new StreamResult(bos);
+			final Transformer xformer = TransformerFactory.newInstance().newTransformer();
+			xformer.transform(source, result);
+			final byte[] xmlData = bos.toByteArray();
+			log.fine(bos.toString());
+			attach.setBinaryData(xmlData);
+			return true;
+		} catch (FolderNotFoundException e) {
+			log.log(Level.SEVERE, "CMIS Root Folder Not Found!");
+		} catch (Exception e) {
+			log.log(Level.SEVERE, "saveLOBData", e);
+		}
+		session = null;
+		attach.setBinaryData(null);
+		return false;
 	}
+
+	/**
+	 * Get Entry Data from one Node
+	 * 
+	 * @param item
+	 */
+	private void getEntryData(Node item) {
+		final Node entryNode = item;
+		if (entryNode == null)
+			return;
+		final NamedNodeMap attributes = entryNode.getAttributes();
+		m_docidNode = attributes.getNamedItem("docid");
+		m_fileNode = attributes.getNamedItem("file");
+		m_nameNode = attributes.getNamedItem("name");
+		m_versionNode = attributes.getNamedItem("version");
+		m_md5Node = attributes.getNamedItem("md5");
+	}
+
+	/**
+	 * Get Entry Data from BinaryData
+	 * @param data
+	 * @param index
+	 */
+	private void getEntryData(byte[] data, int index) {
+
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		try 
+		{
+			// reset all
+			m_docidNode = null;
+			m_fileNode = null;
+			m_nameNode = null;
+			m_versionNode = null;
+			m_md5Node = null;
+			final DocumentBuilder builder = factory.newDocumentBuilder();
+			final Document document = builder.parse(new ByteArrayInputStream(data));
+			final NodeList entries = document.getElementsByTagName("entry");
+			getEntryData(entries.item(index));
+
+		} catch (SAXException sxe) {
+			// Error generated during parsing)
+			Exception x = sxe;
+			if (sxe.getException() != null)
+				x = sxe.getException();
+			x.printStackTrace();
+			log.severe(x.getMessage());
+
+		} catch (ParserConfigurationException pce) {
+			// Parser with specified options can't be built
+			pce.printStackTrace();
+			log.severe(pce.getMessage());
+
+		} catch (IOException ioe) {
+			// I/O error
+			ioe.printStackTrace();
+			log.severe(ioe.getMessage());
+		}
+
+	}
+
+	/**
+	 * Prepare checksum
+	 * 
+	 * @param digest
+	 * @param content
+	 * @return checksum
+	 * @throws IOException
+	 * 
+	 */
+	private String getFileChecksum(MessageDigest digest, byte[] content) throws IOException
+	{
+		// update the digest
+	    digest.update(content); 
+	     
+	    //Get the hash's bytes
+	    byte[] bytes = digest.digest();
+	    
+	    //This bytes[] has bytes in decimal format;
+	    //Convert it to hexadecimal format
+	    StringBuilder sb = new StringBuilder();
+	    for(int i=0; i< bytes.length ;i++)
+	    {
+	        sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+	    }	
+	    
+	    //return complete hash
+	   return sb.toString();
+	}
+	
 
 	/**
 	 * Returns a path snippet, containing client, org, table and record id.
@@ -266,7 +376,7 @@ public class AttachmentAlfresco implements IAttachmentStore {
 			
 			Session session = CmisUtil.createCmisSession(prov.getUserName(), prov.getPassword(), prov.getURL());
 			org.apache.chemistry.opencmis.client.api.Document file = 
-					(org.apache.chemistry.opencmis.client.api.Document) session.getObject(session.createObjectId(getDocId(entry)));
+					(org.apache.chemistry.opencmis.client.api.Document) session.getObject(session.createObjectId(getDocId(attach, i)));
 			
 			log.fine("delete: " + file.getName());
 			if(file !=null){
@@ -283,14 +393,13 @@ public class AttachmentAlfresco implements IAttachmentStore {
 	@Override
 	public boolean deleteEntry(MAttachment attach, MStorageProvider prov, int index) {
 		if (index >= 0 && index < attach.m_items.size()) {
-			// CMIS by KTU
 			//remove files
 			final MAttachmentEntry entry = attach.m_items.get(index);
 
 			// Connect
 			Session session = CmisUtil.createCmisSession(prov.getUserName(), prov.getPassword(), prov.getURL());
 			org.apache.chemistry.opencmis.client.api.Document file = 
-					(org.apache.chemistry.opencmis.client.api.Document) session.getObject(session.createObjectId(getDocId(entry)));
+					(org.apache.chemistry.opencmis.client.api.Document) session.getObject(session.createObjectId(getDocId(attach, index)));
 
 			log.fine("delete: " + file.getName());
 			if(file !=null){
@@ -311,26 +420,58 @@ public class AttachmentAlfresco implements IAttachmentStore {
 	}
 	
 	/**
-	 * @return Returns the docid.
+	 * 
+	 * @param index 
+	 * @return Returns the docId.
 	 */
-	public static String getDocId (MAttachmentEntry entry)
-	{
-		String name = entry.getName();
-		String docid = "";
-		if (name.indexOf("!") > -1)
-			docid = name.split("!")[0];
-		return docid;
-	}
-	
-	/**
-	 * @param name The docid to set.
-	 */
-	private String setDocId (String name, String docid)
-	{
-		if (docid == null)
-			docid = "";
-        name = docid.concat("!").concat(name);
-        return name;
-	}	//	setDocId
+	private String getDocId(MAttachment att, int index) {
 
+		String docId = "";
+		byte[] data = att.getBinaryData();
+		if (data == null)
+			return "";
+		log.fine("TextFileSize=" + data.length);
+		if (data.length == 0)
+			return "";
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		try 
+		{
+			final DocumentBuilder builder = factory.newDocumentBuilder();
+			final Document document = builder.parse(new ByteArrayInputStream(data));
+			final NodeList entries = document.getElementsByTagName("entry");
+			final Node entryNode = entries.item(index);
+			final NamedNodeMap attributes = entryNode.getAttributes();
+			final Node docidNode = attributes.getNamedItem("docid");
+			final Node fileNode = attributes.getNamedItem("file");
+			final Node nameNode = attributes.getNamedItem("name");
+			if(docidNode == null || fileNode==null || nameNode==null){
+				log.severe("no date for entry " + index);
+				att.m_items = null;
+				return "";
+			}
+			log.fine("name: " + nameNode.getNodeValue());
+			docId = docidNode.getNodeValue();
+			log.fine("docId: " + docId);
+		} catch (SAXException sxe) {
+			// Error generated during parsing)
+			Exception x = sxe;
+			if (sxe.getException() != null)
+				x = sxe.getException();
+			x.printStackTrace();
+			log.severe(x.getMessage());
+
+		} catch (ParserConfigurationException pce) {
+			// Parser with specified options can't be built
+			pce.printStackTrace();
+			log.severe(pce.getMessage());
+
+		} catch (IOException ioe) {
+			// I/O error
+			ioe.printStackTrace();
+			log.severe(ioe.getMessage());
+		}
+		return docId;
+	}
+
+	
 }
--- src/org/ecosoft/cmis/CmisUtil.java
+++ src/org/ecosoft/cmis/CmisUtil.java
@@ -5,6 +5,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.chemistry.opencmis.client.api.Document;
@@ -12,6 +13,7 @@ import org.apache.chemistry.opencmis.client.api.Folder;
 import org.apache.chemistry.opencmis.client.api.ItemIterable;
 import org.apache.chemistry.opencmis.client.api.ObjectId;
 import org.apache.chemistry.opencmis.client.api.ObjectType;
+import org.apache.chemistry.opencmis.client.api.Property;
 import org.apache.chemistry.opencmis.client.api.QueryResult;
 import org.apache.chemistry.opencmis.client.api.Repository;
 import org.apache.chemistry.opencmis.client.api.Session;
@@ -22,6 +24,16 @@ import org.apache.chemistry.opencmis.commons.SessionParameter;
 import org.apache.chemistry.opencmis.commons.data.ContentStream;
 import org.apache.chemistry.opencmis.commons.definitions.PropertyDefinition;
 import org.apache.chemistry.opencmis.commons.enums.BindingType;
+import org.apache.chemistry.opencmis.commons.exceptions.CmisObjectNotFoundException;
+import org.apache.chemistry.opencmis.commons.exceptions.CmisRuntimeException;
+
+/**
+ * @author kittiu
+ * @author a42niem - Dirk Niemeyer - action42 GmbH
+ * 
+ * basic work done by kittiu
+ * adapted to Alfresco 5 and extented for versioning 
+ */
 
 public class CmisUtil 
 {
@@ -77,20 +89,66 @@ public class CmisUtil
 	    return document;
 	}
 	
-	public static Document createiDempiereAttachment(Session session, Folder folder, String fileName, String mimetype, byte[] content, String tableName, String recordId) throws Exception 
+	public static Document createiDempiereAttachment(Session session, Folder folder, String fileName, String mimetype, byte[] content, String tableName, String recordId, String checkSum) throws Exception 
 	{
 	    Map<String, Object> docProps = new HashMap<String, Object>();
-	    docProps.put(PropertyIds.NAME, fileName);
-	    docProps.put(PropertyIds.OBJECT_TYPE_ID, "D:id:attachment");
-	    docProps.put("id:tablename", tableName);
-	    docProps.put("id:recordid", recordId);
 	    
 	    ByteArrayInputStream in = new ByteArrayInputStream(content);
 	    ContentStream contentStream = session.getObjectFactory().createContentStream(fileName, content.length, mimetype, in);
-	    
-//	    ObjectId documentId = session.createDocument(docProps, session.createObjectId((String) folder.getPropertyValue(PropertyIds.OBJECT_ID)), contentStream, null, null, null, null);
-//	    Document document = (Document) session.getObject(documentId);
-	    Document document = (Document) folder.createDocument(docProps, contentStream, null);
+	    // check if entry already exists
+	    Document document = null;
+	    try {
+	    	// we better not rely on this path
+	    	// document = (Document) session.getObjectByPath("/" + folder.getName() + "/" + tableName + "/" + recordId + "/" + fileName);
+	    	// so lets query the repository
+	    	StringBuilder query = new StringBuilder("SELECT cmis:objectId FROM id:attachment WHERE ")
+	    		 .append(" id:tablename='").append(tableName)
+	    		 .append("' AND id:recordid='").append(recordId)
+	    	     .append("' AND cmis:name='").append(fileName).append("'");
+	    	ItemIterable<QueryResult> searchResult = session.query(query.toString(), false);
+	    	for (QueryResult resultRow : searchResult) {
+	    		String oldObjectId = (String) resultRow.getPropertyByQueryName("cmis:objectId").getFirstValue();
+	    		document = (Document) session.getObject(oldObjectId);
+	    	}
+	    }
+	    catch (CmisObjectNotFoundException e) {
+	    	;
+	    }
+	    catch (CmisRuntimeException e) {
+	    	;
+	    }
+	    if (document!=null) {
+	    	// make sure we work on latest version
+	    	if (!document.isLatestVersion()) {
+	    		// get latest major version
+	    		document = document.getObjectOfLatestVersion(true); 
+	    	}
+	    	
+	    	Property<Object> objCheckSum = document.getProperty("id:checksum");
+	    	if (objCheckSum.getValue()!=null && objCheckSum.getValue().equals(checkSum)) {
+	    		// we give the existing document back
+	    		return document;
+	    	}
+	    	// else we have a different checksum, so we suppose a new version
+	    	ObjectId pwcId = document.checkOut();   	
+	    	document.refresh();	    	
+	    	Document pwc = (Document) session.getObject(pwcId);
+	    	// we check in the new version with same properties
+	    	ObjectId newDocId = pwc.checkIn(true, null, contentStream, "Another version");
+	    	document = (Document) session.getObject(newDocId);
+	    	// set and update the checksum property
+		    docProps.put("id:checksum", checkSum);
+	    	document.updateProperties(docProps);
+	    }
+	    // nothing found, create new
+	    else {
+		    docProps.put(PropertyIds.NAME, fileName);
+		    docProps.put(PropertyIds.OBJECT_TYPE_ID, "D:id:attachment");
+		    docProps.put("id:tablename", tableName);
+		    docProps.put("id:recordid", recordId);
+		    docProps.put("id:checksum", checkSum);
+	    	document = (Document) folder.createDocument(docProps, contentStream, null);
+	    }
 	    return document;
 	}
   
